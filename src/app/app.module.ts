e93bad9e960cad026b866e2b1370853b8f800214
adamyanvard <vardadamyan@yandex.ru>
2021-12-05 17:39:42 +0400

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/main/header/header.component';
 import {HomeComponent} from "./components/home/home.component";
 
 
@@ -9,7 +10,8 @@ import {HomeComponent} from "./components/home/home.component";
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
