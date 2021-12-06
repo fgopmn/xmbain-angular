88de1dcbd06f7bbdc194f686ddcd2eeda7c8ba02
adamyanvard <vardadamyan@yandex.ru>
2021-12-06 15:49:00 +0400

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/main/header/header.component';
-import {HomeComponent} from "./components/home/home.component";
+import {RouterModule} from "@angular/router";
+
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
