21d724f615cb3f1766c281beff458653d5f3b3bc
ashhoveyan <ash.hoveyan.02@mail.ru>
2021-12-11 13:39:22 +0400

    skzvi verji masy

---
+++ src/app/components/ui-kit/technological/technological.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-technological',
+  templateUrl: './technological.component.html',
+  styleUrls: ['./technological.component.scss']
+})
+export class TechnologicalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
