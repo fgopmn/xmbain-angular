7ca44b0de4ef2981909b337e9f0001316df04b71
Narek.hakobyan <narek.hakobyan.2018@yandex.ru>
2021-12-12 16:43:25 +0400

    new component contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
 import {RouterModule, Routes} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
@@ -7,6 +8,8 @@ import {PressComponent } from './components/press/press.component';
 import {MainModule} from "./components/main/main.module";
 import { ServicesComponent } from './components/services/services.component';
 import {UiKitModule} from "./components/ui-kit/ui-kit.module";
+import { ContactsComponent } from './components/contacts/contacts.component';
+
 
 
 
@@ -21,13 +24,19 @@ const routes: Routes = [
   {
     path:"services",
     component:ServicesComponent
+  },
+  {
+    path:"contacts",
+    component:ContactsComponent
   }
 ];
 @NgModule({
   declarations: [
     AppComponent,
+
     PressComponent,
     ServicesComponent,
+    ContactsComponent,
   ],
   imports: [
     BrowserModule,
