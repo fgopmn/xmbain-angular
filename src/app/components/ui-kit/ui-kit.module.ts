605ad9d98cedf30fff137487d3e9f3c889e9f099
adamyanvard <vardadamyan@yandex.ru>
2021-12-13 17:15:31 +0400

    exhibitions component

--- src/app/components/ui-kit/ui-kit.module.ts
+++ src/app/components/ui-kit/ui-kit.module.ts
@@ -6,13 +6,14 @@ import { TechnologicalComponent } from './technological/technological.component'
 import { PressBlogComponent } from './press-blog/press-blog.component';
 import { PressEventsComponent } from './press-events/press-events.component';
 import { SelectInputComponent } from './select-input/select-input.component';
+import { ExhibitionsComponent } from './exhibitions/exhibitions.component';
 
 
 
 
 @NgModule({
-  declarations: [ComplexComponent, EquipmentComponent, TechnologicalComponent, SelectInputComponent, PressEventsComponent, PressBlogComponent],
-  exports: [ComplexComponent, EquipmentComponent, TechnologicalComponent, PressEventsComponent, PressBlogComponent],
+  declarations: [ComplexComponent, EquipmentComponent, TechnologicalComponent, SelectInputComponent, PressEventsComponent, PressBlogComponent, ExhibitionsComponent],
+  exports: [ComplexComponent, EquipmentComponent, TechnologicalComponent, PressEventsComponent, PressBlogComponent, ExhibitionsComponent],
   imports: [
     CommonModule
   ]
