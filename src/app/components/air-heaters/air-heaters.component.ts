9f40238a0e73e04eee19bc36fc9a8ee23a8a640c
ashhoveyan <ash.hoveyan.02@mail.ru>
2021-12-15 15:06:20 +0400

    varduhi pull exi

---
+++ src/app/components/air-heaters/air-heaters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-air-heaters',
+  templateUrl: './air-heaters.component.html',
+  styleUrls: ['./air-heaters.component.scss']
+})
+export class AirHeatersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
