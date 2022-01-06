315ae879daac558bfd04a2619aa82f5085a08af5
SaqoAvArm <saqo.avetisyan5525@mail.ru>
2022-01-06 17:19:57 +0400

    fix 2 ui-kit block and rename company to product

--- src/app/components/production/production-routing.module.ts
+++ src/app/components/production/production-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {CompanyComponent} from "./company.component";
+import {ProductionComponent} from "./production.component";
 
 const routes: Routes = [
   {
     path:"",
-    component:CompanyComponent
+    component:ProductionComponent
   }
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class CompanyRoutingModule { }
+export class ProductionRoutingModule { }
