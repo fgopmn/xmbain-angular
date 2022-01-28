c4f7f6ff77d6be6f37c9ca1faeb1efa0c4b637e9
ashhoveyan <ash.hoveyan.02@mail.ru>
2022-01-28 13:51:11 +0400

    zut

--- src/app/components/product/product.module.ts
+++ src/app/components/product/product.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
+import {ProductionModule} from "../production/production.module";
+import {ProductComponent} from "./product.component";
+import {MainModule} from "../main/main.module";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductComponent],
   imports: [
     CommonModule,
-    ProductRoutingModule
+    ProductRoutingModule,
+    MainModule
   ]
 })
 export class ProductModule { }
