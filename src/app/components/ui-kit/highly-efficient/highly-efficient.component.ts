1f1e40744015969ddd088d76441b06ac8750dd5e
grigoryanlena <lenagrigoryan1311@mail.ru>
2022-02-27 13:56:28 +0400

    equipment series

--- src/app/components/ui-kit/highly-efficient/highly-efficient.component.ts
+++ src/app/components/ui-kit/highly-efficient/highly-efficient.component.ts
@@ -1,13 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
-
+import {environment} from "../../../../environments/environment.prod";
+import {RequestService} from "../../../service/request.service";
 @Component({
   selector: 'app-highly-efficient',
   templateUrl: './highly-efficient.component.html',
   styleUrls: ['./highly-efficient.component.scss']
 })
 export class HighlyEfficientComponent implements OnInit {
+  @ViewChild('img') img: any;
+  public getList: any[] = [
+  ]
+
+  public image: any[] = [
+    {
+      id:1,
+      img:"../../../../assets/air-heaters/image 2.png"
+    },
+    {
+      id:2,
+      img:"../../../../assets/air-heaters/image.big2.png"
+    },
+    {
+      id:3,
+      img:"../../../../assets/air-heaters/image.big1.png"
+    },
+    {
+      id:4,
+      img:"../../../../assets/air-heaters/image.big3.png"
+    }
+
+    // "../../../../assets/air-heaters/image 2.png",
+    // "../../../../assets/air-heaters/image.big2.png",
+    // "../../../../assets/air-heaters/image.big1.png",
+    // "../../../../assets/air-heaters/image.big3.png",
+  ];
+  public  x: any = "../../../../assets/air-heaters/image 2.png"
 
   public isConsultationOpen:boolean = true;
 
@@ -18,15 +47,31 @@ export class HighlyEfficientComponent implements OnInit {
 
 
   constructor( public fb: FormBuilder,
-               public route: Router) { }
+               public route: Router,
+               public requestService:RequestService) { }
 
   ngOnInit(): void {
+    this.getRequest()
   }
 
 
   public openConsultation(val?:boolean) {
     this.isConsultationOpen = val ? val : !this.isConsultationOpen
   }
+    public openImg(el:any){
+    for (let i=0; i<this.image.length; ++i){
+      if(el.id==this.image[i].id){
+        this.x =this.image[i].img
+      }
 
 
+    }
+
+    }
+
+  getRequest(){
+    this.requestService.getData(`${environment.url}${environment.highlyEfficientImages.get}`).subscribe((items:any) => {
+      this.getList = items
+    })
+  }
 }
